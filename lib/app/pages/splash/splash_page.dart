0362bba28a73f23fce1949e23aa0f5fa0845ff71
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-10-21 18:43:19 -0300

    feat: imports

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fifa_world/app/core/styles/colors_app.dart';
+
+import '../../core/styles/text_styles.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+          titleTextStyle: context.textStyles.textPrimaryFontBold.copyWith(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor:  Colors.white,
+          filled: true,
+          isDense: true,
+          contentPadding: EdgeInsets.all(13),
+          labelStyle: context.textStyles.labelTextField
+        ),
+        primaryColor: context.colors.primary,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: context.colors.primary,
+            primary: context.colors.primary,
+            secondary: context.colors.secondary),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Splash Page'),
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
